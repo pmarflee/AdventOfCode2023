c0a2b75372975c2ae42916124e55e04ea81a75bf
pmarflee <paul.marfleet@gmail.com>
2023-12-04 12:13:05 +0000

    Display total time taken

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -1,4 +1,6 @@
-﻿var table = new Table { Title = new("Advent of Code 2023") };
+﻿using System.Diagnostics;
+
+var table = new Table { Title = new("Advent of Code 2023") };
 
 table.AddColumns(
     new TableColumn("Day"),
@@ -6,9 +8,16 @@ table.AddColumns(
     new TableColumn("Result"),
     new TableColumn("Time Taken"));
 
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
 table.AddRow(1, 1, Day1.SolvePart1);
 table.AddRow(1, 2, Day1.SolvePart2);
 table.AddRow(2, 1, Day2.SolvePart1);
 table.AddRow(2, 2, Day2.SolvePart2);
+
+stopwatch.Stop();
+
+table.AddRow("-", "-", "-", $"[bold lime]{stopwatch.Elapsed}[/]");
 
 AnsiConsole.Write(table);
